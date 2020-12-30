55581cace3b05eb59f37f0a29550aa6e258ec0f9
Fang Li <lifang_1030@hotmail.com>
2020-12-30 18:30:51 +0800

    add firebase admin and graphql

--- rentocal/pages/index.tsx
+++ rentocal/pages/index.tsx
@@ -1,3 +1,11 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../serviceAccount.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
 const Home = () => {
   return (
     <div>Hello World</div>
